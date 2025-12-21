eaba3ccc3d7b64783437d3d732e9c2a6b3f8be33
Ansony <ansonydevx@gmail.com>
2025-12-20 19:38:36 -0500

    fix: corregir CapacidadEntityMapper, quitar mappings innecesarios

--- src/main/java/com/onclass/capacidad/infrastructure/adapters/persistence/mapper/CapacidadEntityMapper.java
+++ src/main/java/com/onclass/capacidad/infrastructure/adapters/persistence/mapper/CapacidadEntityMapper.java
@@ -7,11 +7,8 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CapacidadEntityMapper {
-    @Mapping(target = "tecnologiaIds", ignore = true)
     Capacidad toModel(CapacidadEntity entity);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "nombre", target = "nombre")
-    @Mapping(source = "descripcion", target = "descripcion")
     CapacidadEntity toEntity(Capacidad capacidad);
 }
