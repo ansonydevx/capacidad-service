db2a65e82d919639ceb49b5d88a29f4d4a239d4c
Ansony <ansonydevx@gmail.com>
2025-12-21 20:21:35 -0500

    feat: agregar query port para exponer en hu4

--- src/main/java/com/onclass/capacidad/infrastructure/entrypoints/handler/CapacidadHandler.java
+++ src/main/java/com/onclass/capacidad/infrastructure/entrypoints/handler/CapacidadHandler.java
@@ -4,14 +4,17 @@ import com.onclass.capacidad.domain.api.CapacidadServicePort;
 import com.onclass.capacidad.domain.enums.TechnicalMessage;
 import com.onclass.capacidad.domain.model.Capacidad;
 import com.onclass.capacidad.infrastructure.entrypoints.dto.CapacidadDTO;
+import com.onclass.capacidad.infrastructure.entrypoints.dto.CapacidadExistsRequest;
 import com.onclass.capacidad.infrastructure.entrypoints.dto.CapacidadListado;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CapacidadHandler {
@@ -29,6 +32,13 @@ public class CapacidadHandler {
                 .flatMap(c -> ServerResponse
                         .status(HttpStatus.CREATED)
                         .bodyValue(TechnicalMessage.TECNOLOGIA_CREADA.getMessage()));
+    }
+
+    public Mono<ServerResponse> existen(ServerRequest request) {
+        return request.bodyToMono(CapacidadExistsRequest.class)
+                .doOnNext(req -> log.info("IDs recibidos: {}", req.ids()))
+                .flatMap(req -> capacidadServicePort.existenPorIds(req.ids()))
+                .flatMap(result -> ServerResponse.ok().bodyValue(result));
     }
 
     public Mono<ServerResponse> listar(ServerRequest request) {
